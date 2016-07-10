b0fd58bbf28c589ab3e2224cfb46d988f1317892
hugski <zpf416@hotmail.com>
2016-07-10 23:25:39 +0800

    configure android ndk

---
+++ app/src/main/java/com/zpf416/ar_android/GL3OpenCV.java
@@ -0,0 +1,13 @@
+package com.zpf416.ar_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GL3OpenCV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gl3_open_cv);
+    }
+}
