539f6b8235d6ce9fcdd62a43ee23ddd46c812347
I <zpf416@hotmail.com>
2016-08-08 17:28:16 +0800

    using android studio's new feature of ndk-build

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -4,13 +4,12 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 #include OpenCV SDK
-include ../../../3rd_party/android/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+include /Users/pengfei/github/AR_andorid/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := gl3jni
 LOCAL_CFLAGS    := -Werror
 LOCAL_SRC_FILES := main.cpp Texture.cpp Shader.cpp VideoRenderer.cpp
-LOCAL_LDLIBS    += -llog -lGLESv3 -lm 
-LOCAL_C_INCLUDES +=  Texture.hpp Shader.hpp VideoRenderer.hpp
-
+LOCAL_LDLIBS    += -llog -lGLESv3 -lm
+LOCAL_C_INCLUDES += /usr/local/include
 include $(BUILD_SHARED_LIBRARY)
 
